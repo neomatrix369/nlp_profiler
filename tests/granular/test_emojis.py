854bb7deda793e13e9c1d2dc1cb80c56d695047b
Mani Sarkar <sadhak001@gmail.com>
2020-09-17 07:31:49 +0100

    Tests > Emojis: adding the first two tests on emojis

--- tests/granular/test_emojis.py
+++ tests/granular/test_emojis.py
@@ -3,7 +3,7 @@ from nlp_profiler.core import gather_emojis, count_emojis  # noqa
 text_with_emojis = "I love ⚽ very much 😁"
 
 
-def test_given_text_with_emoji_when_parsed_then_return_only_emojis():
+def test_given_a_text_with_emoji_when_parsed_then_return_only_emojis():
     # given,
     expected_results = ['soccer_ball', 'beaming_face_with_smiling_eyes']
 
@@ -15,7 +15,7 @@ def test_given_text_with_emoji_when_parsed_then_return_only_emojis():
         "Didn't find the two emojis ⚽ and 😁 in the text"
 
 
-def test_given_text_with_emoji_when_counted_then_return_number_of_emojis_found():
+def test_given_a_text_with_emoji_when_counted_then_return_number_of_emojis_found():
     # given, when
     actual_results = count_emojis(text_with_emojis)
 
