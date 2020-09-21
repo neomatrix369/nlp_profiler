17013c1eebaa6f13b3fdeb38fd4fcedb71e02e57
Mani Sarkar <sadhak001@gmail.com>
2020-09-21 15:01:23 +0100

    Refactor: removed duplicate tests in test_grammar_check.py, merged them into a single parameterised test

--- slow-tests/high_level/test_grammar_check.py
+++ slow-tests/high_level/test_grammar_check.py
@@ -14,12 +14,14 @@ text_to_return_value_mapping = [
     (float('nan'), NaN, NOT_APPLICABLE),
     (None, NaN, NOT_APPLICABLE),
     ("", NaN, NOT_APPLICABLE),
+    (no_grammar_issues, 0, "No issues"),
+    (grammar_issues_text, 3, "3 issues"),
 ]
 
 
 @pytest.mark.parametrize("text,expected_score,expected_quality",
                          text_to_return_value_mapping)
-def test_given_an_invalid_text_when_grammar_check_is_applied_then_no_info_is_returned(
+def test_given_a_correct_text_when_grammar_check_is_applied_then_no_grammar_issues_is_returned(
         text: str, expected_score: float, expected_quality: str
 ):
     # given, when: text is not defined
@@ -37,44 +39,6 @@ def test_given_an_invalid_text_when_grammar_check_is_applied_then_no_info_is_ret
     assert actual_quality == expected_quality, \
         f"Grammar quality should NOT " \
         f"have been returned, expected {expected_quality}"
-
-
-def test_given_a_correct_text_when_grammar_check_is_applied_then_no_grammar_issues_is_returned():
-    # given, when
-    actual_results = grammar_check_score(no_grammar_issues)
-
-    # then
-    assert actual_results == 0, \
-        "Grammar check should have passed and returned a good score"
-
-    # given
-    expected_results = 'No issues'
-
-    # when
-    actual_results = grammar_quality(actual_results)
-
-    # then
-    assert expected_results == actual_results, \
-        "Grammar check should have passed with no issues"
-
-
-def test_given_a_text_when_grammar_check_is_applied_then_grammar_check_analysis_info_is_returned():
-    # given, when
-    actual_results = grammar_check_score(grammar_issues_text)
-
-    # then
-    assert actual_results == 3, \
-        "Grammar check score didn't match for the text"
-
-    # given
-    expected_results = '3 issues'
-
-    # when
-    actual_results = grammar_quality(actual_results)
-
-    # then
-    assert expected_results == actual_results, \
-        "Grammar check didn't match for the text"
 
 
 grammar_check_score_to_words_mapping = [
