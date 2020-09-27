17622c5b2fcfbf6bef9859ebf15eade29dbef22c
Mani Sarkar <sadhak001@gmail.com>
2020-09-27 11:39:53 +0100

    Release: correcting the topics in setup.py

---
+++ release-to-pypi.sh
@@ -0,0 +1,40 @@
+
+
+#!/bin/bash
+
+#
+# Copyright 2020 Mani Sarkar
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+set -u
+set -o pipefail
+
+echo ""
+echo "Creating the distribution artifacts"
+echo "  -- removing older version of build, dist and nlp_profiler.egg-info"
+rm -fr build dist nlp_profiler.egg-info
+echo "  -- running python setup.py"
+python setup.py sdist
+
+echo ""
+echo "Uploading package using twine"
+twine upload dist/*
+echo ""
+
+PACKAGE_VERSION=$(grep  "__version__ = " nlp_profiler/__init__.py | awk '{print $3}' | tr -d '"' || true)
+echo "~~~ Finished uploading nlp_profiler ${PACKAGE_VERSION} to pypy ~~~"
+echo "~~~ Goto https://pypi.org/project/nlp-profiler/{PACKAGE_VERSION}/ to see latest version ~~~"
+echo ""
