f8a22baf24c39e58f2c8f7cb3faecdb6b87f8462
Ritik Jain <ritik@pichainlabs.com>
2020-10-06 10:48:35 +0530

    Added Noun phrase count

--- nlp_profiler/granular_features/__init__.py
+++ nlp_profiler/granular_features/__init__.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-from nlp_profiler.granular_features.alphanumeric import count_alpha_numeric
-from nlp_profiler.granular_features.chars_and_spaces \
-    import count_spaces, count_chars, count_characters_excluding_spaces
 from nlp_profiler.constants import \
     ALPHA_NUMERIC_COUNT_COL, WHOLE_NUMBERS_COUNT_COL, EMOJI_COUNT_COL, CHARS_EXCL_SPACES_COUNT_COL
 from nlp_profiler.constants import CHARACTERS_COUNT_COL, SENTENCES_COUNT_COL
@@ -10,11 +7,14 @@ from nlp_profiler.constants import \
     DATES_COUNT_COL, STOP_WORDS_COUNT_COL, PUNCTUATIONS_COUNT_COL, NON_ALPHA_NUMERIC_COUNT_COL
 from nlp_profiler.constants import \
     DEFAULT_PARALLEL_METHOD
-from nlp_profiler.constants import DUPLICATES_COUNT_COL, COUNT_WORDS_COL, SPACES_COUNT_COL
+from nlp_profiler.constants import DUPLICATES_COUNT_COL, SPACES_COUNT_COL
+from nlp_profiler.generate_features import generate_features
+from nlp_profiler.granular_features.alphanumeric import count_alpha_numeric
+from nlp_profiler.granular_features.chars_and_spaces \
+    import count_spaces, count_chars, count_characters_excluding_spaces
 from nlp_profiler.granular_features.dates import count_dates
 from nlp_profiler.granular_features.duplicates import count_duplicates
 from nlp_profiler.granular_features.emojis import count_emojis
-from nlp_profiler.generate_features import generate_features
 from nlp_profiler.granular_features.non_alphanumeric import count_non_alpha_numeric
 from nlp_profiler.granular_features.numbers import count_whole_numbers
 from nlp_profiler.granular_features.punctuations import count_punctuations
@@ -41,6 +41,7 @@ def apply_granular_features(heading: str,
         (PUNCTUATIONS_COUNT_COL, text_column, count_punctuations),
         (STOP_WORDS_COUNT_COL, text_column, count_stop_words),
         (DATES_COUNT_COL, text_column, count_dates),
+        (NOUN_PHASE_COUNT_COL, text_column, count_phase)
     ]
     generate_features(
         heading, granular_features_steps,
