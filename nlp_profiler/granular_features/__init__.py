aa28b360653c640a954059bb82174089a8eb3af8
Ritik Jain <ritik@pichainlabs.com>
2020-10-06 18:04:06 +0530

    Noun Phase added in notebook, function renamed and requirements updated

--- nlp_profiler/granular_features/__init__.py
+++ nlp_profiler/granular_features/__init__.py
@@ -7,7 +7,7 @@ from nlp_profiler.constants import \
     DATES_COUNT_COL, STOP_WORDS_COUNT_COL, PUNCTUATIONS_COUNT_COL, NON_ALPHA_NUMERIC_COUNT_COL
 from nlp_profiler.constants import \
     DEFAULT_PARALLEL_METHOD
-from nlp_profiler.constants import DUPLICATES_COUNT_COL, SPACES_COUNT_COL
+from nlp_profiler.constants import DUPLICATES_COUNT_COL, WORDS_COUNT_COL, SPACES_COUNT_COL, NOUN_PHASE_COUNT_COL
 from nlp_profiler.generate_features import generate_features
 from nlp_profiler.granular_features.alphanumeric import count_alpha_numeric
 from nlp_profiler.granular_features.chars_and_spaces \
@@ -31,7 +31,7 @@ def apply_granular_features(heading: str,
         (SENTENCES_COUNT_COL, text_column, count_sentences),
         (CHARACTERS_COUNT_COL, text_column, count_chars),
         (SPACES_COUNT_COL, text_column, count_spaces),
-        (COUNT_WORDS_COL, text_column, count_words),
+        (WORDS_COUNT_COL, text_column, count_words),
         (DUPLICATES_COUNT_COL, text_column, count_duplicates),
         (CHARS_EXCL_SPACES_COUNT_COL, text_column, count_characters_excluding_spaces),
         (EMOJI_COUNT_COL, text_column, count_emojis),
@@ -41,7 +41,7 @@ def apply_granular_features(heading: str,
         (PUNCTUATIONS_COUNT_COL, text_column, count_punctuations),
         (STOP_WORDS_COUNT_COL, text_column, count_stop_words),
         (DATES_COUNT_COL, text_column, count_dates),
-        (NOUN_PHASE_COUNT_COL, text_column, count_phase)
+        (NOUN_PHASE_COUNT_COL, text_column, count_noun_phase)
     ]
     generate_features(
         heading, granular_features_steps,
--- tests/granular/test_nounphase.py
+++ tests/granular/test_nounphase.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from nlp_profiler.noun_phase_count import count_phase, gather_nouns
+from nlp_profiler.noun_phase_count import count_noun_phase, gather_nouns
 from nlp_profiler.constants import NaN
 
 
@@ -47,7 +47,7 @@ text_to_return_value_mapping = [
                          text_to_return_value_mapping)
 def test_count_noun(text: str, expected_result: str): 
 
-    actual_result = count_phase(text)
+    actual_result = count_noun_phase(text)
     
     assert actual_result == expected_result, \
     f"Expected Result: {expected_result} \
@@ -61,7 +61,7 @@ text_to_return_value_mapping = [
                          text_to_return_value_mapping)
 def test_failed_count_noun(text: str, expected_result: str): 
 
-    actual_result = count_phase(text)
+    actual_result = count_noun_phase(text)
     
     assert actual_result == expected_result, \
     f"Expected Result: {expected_result} \
