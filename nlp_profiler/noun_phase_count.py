57d178fdf795892b122f3a429beb1b7bd05f80e9
Ritik Jain <ritik@pichainlabs.com>
2020-10-07 18:53:51 +0530

    TestCases filteration and emoji decoder added

--- nlp_profiler/noun_phase_count.py
+++ nlp_profiler/noun_phase_count.py
@@ -1,15 +1,17 @@
-import en_core_web_sm
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
 from nlp_profiler.constants import NaN
-nlp = en_core_web_sm.load()
-
+import re, emoji
 
 def gather_nouns(sentence: str):
 
     if not isinstance(sentence, str) or len(sentence) == 0:
         return []
-    
-    doc = nlp(sentence)
-    return [(x.text, x.label_) for x in doc.ents]
+    emoji_decoded = emoji.demojize(sentence, delimiters=("", "")).lower().strip() # Decoding Emoji's
+    # import pdb; pdb.set_trace();
+    token = word_tokenize(emoji_decoded)
+    tags =  list(filter(lambda x: re.match(r"(JJ|NN|NNP)", x[1]), pos_tag(token))) # using RegEx to to check for Noun Phases.
+    return tags
 
 
 def count_noun_phase(text: str) -> int:
