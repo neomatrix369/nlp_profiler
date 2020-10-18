b3592de9d110fc5836a8b3db204e800dd79d85e6
Mani Sarkar <sadhak001@gmail.com>
2020-10-18 13:28:02 +0100

    Noun Phrase count: download the nltk component: averaged_perceptron_tagger in order for the functionality to work

--- nlp_profiler/granular_features/noun_phase_count.py
+++ nlp_profiler/granular_features/noun_phase_count.py
@@ -1,16 +1,23 @@
-from nltk.tokenize import word_tokenize
+import re
+
+import emoji
+import nltk
 from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+
 from nlp_profiler.constants import NaN
-import re, emoji
 
-def gather_nouns(sentence:str):
+nltk.download('averaged_perceptron_tagger')
 
+
+def gather_nouns(sentence: str):
     if not isinstance(sentence, str) or len(sentence) == 0:
         return []
-    emoji_decoded = emoji.demojize(sentence, delimiters=("", "")).lower().strip() # Decoding Emoji's
+    emoji_decoded = emoji.demojize(sentence, delimiters=("", "")).lower().strip()  # Decoding Emoji's
     # import pdb; pdb.set_trace();
     token = word_tokenize(emoji_decoded)
-    tags =  list(filter(lambda x: re.match(r"(JJ|NN|NNP)", x[1]), pos_tag(token))) # using RegEx to to check for Noun Phases.
+    tags = list(
+        filter(lambda x: re.match(r"(JJ|NN|NNP)", x[1]), pos_tag(token)))  # using RegEx to to check for Noun Phases.
     return tags
 
 
