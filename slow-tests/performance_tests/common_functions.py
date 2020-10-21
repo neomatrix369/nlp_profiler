7eb418c70de71378a86b1e6515a31164ddb1fbe6
Mani Sarkar <sadhak001@gmail.com>
2020-10-21 13:15:55 +0100

    Refactor: extracting the common aspects of the test into two parts, creating generic function from the slow tests (test_perf_grammar_check.py)

--- slow-tests/performance_tests/common_functions.py
+++ slow-tests/performance_tests/common_functions.py
@@ -1,4 +1,45 @@
+import os
+from contextlib import redirect_stdout
+from datetime import datetime
+from time import time
+
 import git
+from line_profiler import LineProfiler
+
+CURRENT_SOURCE_FILEPATH = os.path.abspath(__file__)
+EXPECTED_DATA_PATH = f'{os.path.dirname(CURRENT_SOURCE_FILEPATH)}/data'
+TARGET_PROFILE_REPORT_FOLDER = '.cprofile/'
+
+
+def assert_benchmark(expected_execution_time: float,
+                     target_function,
+                     profile_filename_prefix: str,
+                     commit_id: str):
+    # given
+    if not os.path.exists(TARGET_PROFILE_REPORT_FOLDER):
+        os.makedirs(TARGET_PROFILE_REPORT_FOLDER)
+    profile = LineProfiler()
+    source_data = generate_data()
+
+    # when: using default method (joblib Parallel) for parallelisation
+    start_execution_time = time()
+    profile_wrapper = profile(target_function)
+    for each in source_data:
+        profile_wrapper(each)
+    actual_execution_time = time() - start_execution_time
+    output_filename = f'{TARGET_PROFILE_REPORT_FOLDER}/{profile_filename_prefix}-' \
+                      f'{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}-' \
+                      f'{shorten_sha(git_current_head_sha())}'
+    with open(f'{output_filename}.txt', 'w') as file:
+        with redirect_stdout(file):
+            profile.print_stats()
+    profile.dump_stats(f'{output_filename}.lprof')
+
+    # then
+    assert actual_execution_time <= expected_execution_time, \
+        f"Expected duration: {expected_execution_time}, Actual duration: {actual_execution_time}. " \
+        f"Slow down by: {abs(actual_execution_time - expected_execution_time)} seconds. " \
+        f"We have crossed the benchmark limit after a speed up via commit {commit_id}."
 
 
 def shorten_sha(long_sha):
--- slow-tests/performance_tests/test_perf_grammar_check.py
+++ slow-tests/performance_tests/test_perf_grammar_check.py
@@ -1,13 +1,8 @@
 import os
-from contextlib import redirect_stdout
-from datetime import datetime
-from time import time
-
-from line_profiler import LineProfiler
 
 from nlp_profiler.high_level_features.grammar_quality_check \
     import grammar_check_score
-from .common_functions import shorten_sha, git_current_head_sha, generate_data
+from .common_functions import assert_benchmark
 
 CURRENT_SOURCE_FILEPATH = os.path.abspath(__file__)
 EXPECTED_DATA_PATH = f'{os.path.dirname(CURRENT_SOURCE_FILEPATH)}/data'
@@ -15,31 +10,13 @@ TARGET_PROFILE_REPORT_FOLDER = '.cprofile/'
 
 
 def test_given_a_text_column_when_profiler_is_applied_with_high_level_analysis_then_it_finishes_quick():
-    # given
-    if not os.path.exists(TARGET_PROFILE_REPORT_FOLDER):
-        os.makedirs(TARGET_PROFILE_REPORT_FOLDER)
-    profile = LineProfiler()
-    source_data = generate_data()
-    expected_execution_time = 4  # benchmarked: (first-time) 46.694923639297485, (cached) 5.918392 seconds
+    # benchmarked:
+    #   (first-time): 46.694923639297485,
+    #   (cached): 5.918392 seconds
+    #   (new run at 5088e8d): 12.149042129516602
+    expected_execution_time = 13
 
-    # when: using default method (joblib Parallel) for parallelisation
-    start_execution_time = time()
-    profile_wrapper = profile(grammar_check_score)
-    for each in source_data:
-        profile_wrapper(each)
-    actual_execution_time = time() - start_execution_time
-
-    output_filename = f'{TARGET_PROFILE_REPORT_FOLDER}/grammar_check_score-' \
-                      f'{datetime.now().strftime("%d-%m-%Y-%H-%M-%S")}-' \
-                      f'{shorten_sha(git_current_head_sha())}'
-    with open(f'{output_filename}.txt', 'w') as file:
-        with redirect_stdout(file):
-            profile.print_stats()
-
-    profile.dump_stats(f'{output_filename}.lprof')
-
-    # then
-    assert actual_execution_time <= expected_execution_time, \
-        f"Expected duration: {expected_execution_time}, Actual duration: {actual_execution_time}. " \
-        f"Slow down by: {abs(actual_execution_time - expected_execution_time)} seconds. " \
-        f"We have crossed the benchmark limit after a speed up via commit 51a8952."
+    assert_benchmark(expected_execution_time,
+                     grammar_check_score,
+                     'grammar_check_score',
+                     '51a8952')
