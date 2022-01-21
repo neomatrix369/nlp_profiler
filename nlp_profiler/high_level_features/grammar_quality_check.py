d38430bfa1b9469e4b544dcf818ce6e527ec0a40
Eduardo De Leon <eedeleon@Eduardos-MBP.attlocal.net>
2022-01-20 20:14:08 -0500

    Remove copy left package

--- nlp_profiler/high_level_features/grammar_quality_check.py
+++ nlp_profiler/high_level_features/grammar_quality_check.py
@@ -1,6 +1,5 @@
-import language_tool_python
+# Modified: Removed use and installation of language_tool_python, LICENSE is copy left
 
-language_tool = language_tool_python.LanguageTool('en-GB')
 import pandas as pd
 import math
 
@@ -14,23 +13,12 @@ def apply_grammar_check(heading: str,
                         text_column: dict,
                         parallelisation_method: str = DEFAULT_PARALLEL_METHOD):
     grammar_checks_steps = [
-        (GRAMMAR_CHECK_SCORE_COL, text_column, grammar_check_score),
         (GRAMMAR_CHECK_COL, GRAMMAR_CHECK_SCORE_COL, grammar_quality),
     ]
     generate_features(
         heading, grammar_checks_steps,
         new_dataframe, parallelisation_method
     )
-
-
-### Grammar check: this is a very slow process
-### take a lot of time per text it analysis
-def grammar_check_score(text: str) -> int:
-    if (not isinstance(text, str)) or (len(text.strip()) == 0):
-        return NaN
-
-    matches = language_tool.check(text)
-    return len(matches)
 
 
 def grammar_quality(score: int) -> str:
