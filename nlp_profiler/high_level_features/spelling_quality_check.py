d292883deadd7ef7726e85589ed66e8eb3caf700
Bitan Biswas <bitanbiswas99@gmail.com>
2023-03-11 22:58:07 +0530

    spelling checker modified

--- nlp_profiler/high_level_features/spelling_quality_check.py
+++ nlp_profiler/high_level_features/spelling_quality_check.py
@@ -3,8 +3,25 @@ import tempfile
 
 import pandas as pd
 from joblib import Memory
-from nltk.tokenize import word_tokenize
-from textblob import Word
+
+#alternative for textblob spell checker
+from symspellpy import SymSpell, Verbosity
+import pkg_resources
+
+# load a dictionary (this one consists of 82,765 English words)
+#there are other dictionaries as well look at https://www.kaggle.com/code/muhammetfaik/spellcheckingbenchmark
+#link to the used frequency dictionary https://raw.githubusercontent.com/wolfgarbe/SymSpell/master/SymSpell/frequency_dictionary_en_82_765.txt
+sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
+dictionary_path = pkg_resources.resource_filename(
+    "symspellpy", "frequency_dictionary_en_82_765.txt"
+)
+# term_index: column of the term 
+# count_index: column of the term's frequency
+sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
+
+#importing fuzzy matching for scoring purposes
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 from nlp_profiler.constants import \
     DEFAULT_PARALLEL_METHOD
@@ -38,13 +55,30 @@ spelling_quality_score_to_words_mapping = [
     ["Very good", 99, 100],  # Very good: Certain: 100%: Give or take 0%
     ["Quite good", 97, 99],  # Quite Good: Almost Certain: 93%: Give or take 6%
     ["Good", 95, 97],  # Quite Good: Almost Certain: 93%: Give or take 6%
-    ["Pretty good", 90, 95],  # Pretty: Good: Probable: 75%: Give or take about 12%
-    ["Bad", 60, 90],  # So/so: Chances About Even: 50%: Give or take about 10%
+    ["Pretty good", 80, 95],  # Pretty: Good: Probable: 75%: Give or take about 12%
+    ["Bad", 60, 80],  # So/so: Chances About Even: 50%: Give or take about 10%
     ["Pretty bad", 12, 60],  # Pretty bad: Probably Not: 30%: Give or take about 10%
     ["Quite bad", 2, 12],  # Quite bad: Almost Certainly Not 7%: Give or take about 5%
     ["Very bad", 0, 2]  # Impossible 0%: Give or take 0%
 ]
 
+##additional function for spelling check and alternative building. Here the chekcs take punctuations into account as well.
+##For instance,"badrestaurant" is converted to "bad restaurant" and the score is affected due to the same 
+def symspell_corrector(input_term: str) -> str:
+  # look up suggestions for multi-word input strings 
+    suggestions = sym_spell.lookup_compound( 
+        phrase=input_term,  
+        max_edit_distance=2,  
+        transfer_casing=True,  
+        ignore_term_with_digits=True, 
+        ignore_non_words=True, 
+        split_by_space=True 
+    ) 
+    # display the correction
+    for suggestion in suggestions: 
+      if suggestion.term:
+        return suggestion.term
+    return input_term
 
 def spelling_quality_summarised(quality: str) -> str:
     if (not quality) or (quality == NOT_APPLICABLE):
@@ -57,32 +91,18 @@ def spelling_quality_summarised(quality: str) -> str:
 
 
 def spelling_quality_score(text: str) -> float:
-    if (not isinstance(text, str)) or (len(text.strip()) == 0):
+    result=0.0
+    if not isinstance(text, str) or len(text.strip()) == 0:
         return NaN
 
-    tokenized_text = get_tokenized_text(text)
-    misspelt_words = [
-        each_word for _, each_word in enumerate(tokenized_text)
-        if actual_spell_check(each_word) is not None
-    ]
-    result = 1 - (len(misspelt_words) / len(tokenized_text))
+    corrected_text = symspell_corrector(text) 
+    if corrected_text is not None:
+      result = fuzz.token_sort_ratio(corrected_text,text)/100
 
     return result if result >= 0.0 else 0.0
 
 
-def get_tokenized_text(text: str) -> list:
-    cached_function = memory.cache(word_tokenize)
-    return cached_function(text.lower())
-
-
 @memory.cache
-def actual_spell_check(each_word: str) -> str:  # pragma: no cover
-    # pragma: no cover => as multiprocess leads to loss of test coverage info
-    spellchecked_word = Word(each_word).spellcheck()
-    _, score = spellchecked_word[0]
-    return each_word if score != 1 else None
-
-
 def spelling_quality(score: float) -> str:
     if math.isnan(score):
         return NOT_APPLICABLE
