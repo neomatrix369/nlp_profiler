929829939482d306fcaaa4cc2985c5e77e8ebf58
Bitan Biswas <bitanbiswas99@gmail.com>
2023-03-12 23:40:43 +0530

    code cleaned with black

--- nlp_profiler/high_level_features/grammar_quality_check.py
+++ nlp_profiler/high_level_features/grammar_quality_check.py
@@ -1,26 +1,28 @@
-#changing the grammar checker from language tool to gingerit for better results
+# changing the grammar checker from language tool to gingerit for better results
 from gingerit.gingerit import GingerIt
+
 parser = GingerIt()
 import pandas as pd
 import math
 
-from nlp_profiler.constants import NOT_APPLICABLE, NaN, DEFAULT_PARALLEL_METHOD, \
-    GRAMMAR_CHECK_SCORE_COL, GRAMMAR_CHECK_COL
+from nlp_profiler.constants import (
+    NOT_APPLICABLE,
+    NaN,
+    DEFAULT_PARALLEL_METHOD,
+    GRAMMAR_CHECK_SCORE_COL,
+    GRAMMAR_CHECK_COL,
+)
 from nlp_profiler.generate_features import generate_features
 
 
-def apply_grammar_check(heading: str,
-                        new_dataframe: pd.DataFrame,
-                        text_column: dict,
-                        parallelisation_method: str = DEFAULT_PARALLEL_METHOD):
+def apply_grammar_check(
+    heading: str, new_dataframe: pd.DataFrame, text_column: dict, parallelisation_method: str = DEFAULT_PARALLEL_METHOD
+):
     grammar_checks_steps = [
         (GRAMMAR_CHECK_SCORE_COL, text_column, grammar_check_score),
         (GRAMMAR_CHECK_COL, GRAMMAR_CHECK_SCORE_COL, grammar_quality),
     ]
-    generate_features(
-        heading, grammar_checks_steps,
-        new_dataframe, parallelisation_method
-    )
+    generate_features(heading, grammar_checks_steps, new_dataframe, parallelisation_method)
 
 
 ### Grammar check: this is a very slow process
@@ -28,9 +30,9 @@ def apply_grammar_check(heading: str,
 def grammar_check_score(text: str) -> int:
     if not (isinstance(text, str) or text.strip()):
         return NaN
-    #calling the parser function to parse through the text for errors
+    # calling the parser function to parse through the text for errors
     matches = parser.parse(text)
-    #the corrections is an array of dictionaries containing the position and the word that has been changed
+    # the corrections is an array of dictionaries containing the position and the word that has been changed
     return len(matches["corrections"])
 
 
