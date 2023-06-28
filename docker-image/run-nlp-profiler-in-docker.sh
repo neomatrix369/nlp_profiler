a163d75e30219c93129c23e56a9e0c836c4cc81a
lilianabs <lilianabsmath@gmail.com>
2023-06-27 21:16:47 -0600

    Make Python version configurable

--- docker-image/run-nlp-profiler-in-docker.sh
+++ docker-image/run-nlp-profiler-in-docker.sh
@@ -7,11 +7,13 @@ set -o pipefail
 MOUNT_VOLUME="${PWD}/../"
 TARGET_FOLDER="/nlp_profiler"
 DOCKER_IMAGE_NAME="nlp_profiler"
+PYTHON_VERSION="3.8"
 
 echo "~~~ Running nlp_profiler in a docker container"
 echo "Mounted volume: ${MOUNT_VOLUME}"
+echo "Python version: ${PYTHON_VERSION}"
 
-docker build -t ${DOCKER_IMAGE_NAME} .
+docker build -t ${DOCKER_IMAGE_NAME} --build-arg python_version=${PYTHON_VERSION} .
 
 docker run -it                                       \
            --volume ${MOUNT_VOLUME}:${TARGET_FOLDER} \
