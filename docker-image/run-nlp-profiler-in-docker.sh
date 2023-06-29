b32a8fbee6129490e4f53038112cf504fcd12c75
lilianabs <lilianabsmath@gmail.com>
2023-06-28 22:30:52 -0600

    Use local folder

--- docker-image/run-nlp-profiler-in-docker.sh
+++ docker-image/run-nlp-profiler-in-docker.sh
@@ -10,10 +10,12 @@ DOCKER_IMAGE_NAME="nlp_profiler"
 PYTHON_VERSION="3.8"
 
 echo "~~~ Running nlp_profiler in a docker container"
+echo "Docker image name: " ${DOCKER_IMAGE_NAME}}
 echo "Mounted volume: ${MOUNT_VOLUME}"
+echo "Target folder: " ${TARGET_FOLDER}
 echo "Python version: ${PYTHON_VERSION}"
 
-docker build -t ${DOCKER_IMAGE_NAME} --build-arg python_version=${PYTHON_VERSION} .
+docker build -t ${DOCKER_IMAGE_NAME} --build-arg python_version=${PYTHON_VERSION} ../
 
 docker run -it                                       \
            --volume ${MOUNT_VOLUME}:${TARGET_FOLDER} \
