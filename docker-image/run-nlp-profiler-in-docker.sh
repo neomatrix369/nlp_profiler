00b6247f557c50e3bad20d1afe2d9c5b603c0445
lilianabs <lilianabsmath@gmail.com>
2023-07-07 18:49:24 -0600

    Format Dockerfile and shell script

--- docker-image/run-nlp-profiler-in-docker.sh
+++ docker-image/run-nlp-profiler-in-docker.sh
@@ -4,21 +4,25 @@ set -e
 set -u
 set -o pipefail
 
+PYTHON_VERSION=${1:-"3.8"}
+
 MOUNT_VOLUME="${PWD}/../"
 TARGET_FOLDER="/nlp_profiler"
 DOCKER_IMAGE_NAME="nlp_profiler"
-PYTHON_VERSION="3.8"
 
 echo "~~~ Running nlp_profiler in a docker container"
-echo "Docker image name: " ${DOCKER_IMAGE_NAME}}
+echo "Docker image name: ${DOCKER_IMAGE_NAME}"
 echo "Mounted volume: ${MOUNT_VOLUME}"
-echo "Target folder: " ${TARGET_FOLDER}
-echo "Python version: ${PYTHON_VERSION}"
+echo "Target folder:  ${TARGET_FOLDER}"
 
-docker build -t ${DOCKER_IMAGE_NAME} --build-arg python_version=${PYTHON_VERSION} ../
+echo "~~~ Building docker image ${DOCKER_IMAGE_NAME} with Python version ${PYTHON_VERSION}."
+docker build -t ${DOCKER_IMAGE_NAME} --build-arg python_version="${PYTHON_VERSION}" ../
 
+echo "~~~ Running docker container ${DOCKER_IMAGE_NAME}."
 docker run -it                                       \
-           --volume ${MOUNT_VOLUME}:${TARGET_FOLDER} \
+           --volume "${MOUNT_VOLUME}":${TARGET_FOLDER} \
            --workdir ${TARGET_FOLDER}                \
            ${DOCKER_IMAGE_NAME}                      \
            /bin/bash
+
+echo "~~~ Exited docker container."
